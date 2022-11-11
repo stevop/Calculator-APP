bea43b4b51b97f32e6390b47cbf3a2cff1bae80e
StefanPacola <stevo.pacola@gmail.com>
2022-11-11 21:17:17 +0100

    buttons now updates tip amount and total amount to pay per person

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,7 @@
 const billInput = document.getElementById("bill");
 const peopleInput = document.getElementById("num-of-people");
 const reset = document.querySelector(".reset-btn");
+let tipBtn = document.querySelectorAll(".tip");
 const tip5percent = document.querySelector(".tip5");
 const tip10percent = document.querySelector(".tip10");
 const tip15percent = document.querySelector(".tip15");
@@ -10,7 +11,23 @@ const tip50percent = document.querySelector(".tip50");
 const tipPerPerson = document.querySelector(".tip-amount-num");
 const totalPerPerson = document.querySelector(".total-amount-num");
 function updateTotal() {
-    let billValue = billInput.value;
-    totalPerPerson.textContent = "$" + parseFloat(billValue);
+    let billValue = parseFloat(billInput.value);
+    let tipValue = parseFloat(tipPerPerson.innerHTML.slice(1));
+    let peopleValue = peopleInput.value;
+    totalPerPerson.textContent = `$${(billValue + tipValue) / peopleValue}`;
 }
+function resetValues() {
+    billInput.value = 0;
+    peopleInput.value = 1;
+    updateTotal();
+}
+reset === null || reset === void 0 ? void 0 : reset.addEventListener("click", resetValues);
+tipBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        tipPerPerson.textContent =
+            "$" +
+                (billInput.value * (parseFloat(btn.innerHTML) / 100)) / peopleInput.value;
+        updateTotal();
+    });
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ const billInput: any = document.getElementById("bill");
 const peopleInput: any = document.getElementById("num-of-people");
 const reset = document.querySelector(".reset-btn");
 
+let tipBtn = document.querySelectorAll(".tip");
 const tip5percent = document.querySelector(".tip5");
 const tip10percent = document.querySelector(".tip10");
 const tip15percent = document.querySelector(".tip15");
@@ -11,9 +12,26 @@ const tip50percent = document.querySelector(".tip50");
 const tipPerPerson = document.querySelector(".tip-amount-num");
 const totalPerPerson: any = document.querySelector(".total-amount-num");
 
-
-// THIS WORKS!
 function updateTotal() {
-	let billValue = billInput.value;
-	totalPerPerson!.textContent = "$" + parseFloat(billValue);
+	let billValue = parseFloat(billInput.value);
+	let tipValue = parseFloat(tipPerPerson!.innerHTML.slice(1));
+	let peopleValue = peopleInput.value;
+	totalPerPerson!.textContent = `$${(billValue + tipValue) / peopleValue}`;
 }
+
+function resetValues() {
+	billInput.value = 0;
+	peopleInput.value = 1;
+	updateTotal();
+}
+
+reset?.addEventListener("click", resetValues);
+
+tipBtn.forEach((btn) => {
+	btn.addEventListener("click", () => {
+		tipPerPerson!.textContent =
+			"$" +
+			(billInput.value * (parseFloat(btn.innerHTML) / 100)) / peopleInput.value;
+		updateTotal();
+	});
+});
